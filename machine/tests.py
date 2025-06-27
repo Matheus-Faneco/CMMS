264b8eba619c71a5fe22450d8a42ab25b4f4a935
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-06-27 14:55:43 -0400

    Adiciona teste para adiçao de maquina

--- machine/tests.py
+++ machine/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Machine
 
-# Create your tests here.
+class MachineTests(APITestCase):
+    #Teste de Adição de máquina
+    def test_add_machine(self):
+        url = reverse('machine-list')
+        data = {
+            "name": "Compressor A",
+            "location": "Fábrica 1",
+            "status": "O"
+            }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Machine.objects.count(), 1)
+        self.assertEqual(Machine.objects.get().name, 'Compressor A')
+
