e48506efc609ad3665d8c267619cbf5300fcce78
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-07-08 02:53:42 -0400

    testes de orders

--- order/models.py
+++ order/models.py
@@ -1,6 +1,4 @@
-
 from django.db import models
-
 from machine.models import Machine
 from maintenance_worker.models import MaintenanceWorker
 
--- order/tests.py
+++ order/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from user.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from machine.models import Machine
+from maintenance_worker.models import MaintenanceWorker
 
-# Create your tests here.
+
+class OrdersAPITests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='M1235', password='123')
+        refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(refresh.access_token)
+
+        self.machine = Machine.objects.create(
+            name="Lathe X300",
+            location="Sector A",
+            status='O'
+        )
+
+        self.worker = MaintenanceWorker.objects.create(
+            worker_name="Carlos Silva",
+            worker_role="Técnico Mecânico"
+        )
+        self.url = '/order/'
+
+    def test_no_auth(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_orders_with_auth(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_order_with_auth(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        data = {
+            "fk_machine": self.machine.id,
+            "fk_maintenance_worker": self.worker.id,
+            "order_description": "Trocar filtro",
+            "order_type": "P",
+            "order_status": "O"
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["order_description"], "Trocar filtro")
