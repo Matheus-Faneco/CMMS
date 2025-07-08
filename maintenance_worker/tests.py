f4096754f3085f4e924fbea16a5a588c44824a54
Matheus-Faneco <matheusfaneco2009@hotmail.com>
2025-07-08 03:13:55 -0400

    comentarios nos testes de de maintenance_worker

--- maintenance_worker/tests.py
+++ maintenance_worker/tests.py
@@ -3,16 +3,20 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from user.models import User
 
 class MaintenanceWorkerTests(APITestCase):
+
+    #set up by creating a user and setting the maintenance worker endpoint
     def setUp(self):
         self.user = User.objects.create_user(username='user', password='123')
         refresh = RefreshToken.for_user(self.user)
         self.token = str(refresh.access_token)
         self.url = '/maintenance_worker/'
 
+    # test accessing the endpoint without passing credentials in the header
     def test_list_requires_auth(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 401)
 
+    # test accessing the endpoint with credentials and creating a worker
     def test_create_worker_authenticated(self):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
         data = {
